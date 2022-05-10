1d004c7d720fc08f38382eac67daea18a88c9dbe
곽경희 <82927462+kwakkyunghee@users.noreply.github.com>
2022-05-11 00:00:08 +0900

    바닥 움직이기, 캐릭터 날아가기..

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    // public float speed;
+    // public int startIndex;
+    //public int endIndex;
+    // public Transform[] sprites;
+
+    //  float viewHeight;
+
+    public float speed;
+    public Transform[] backgrounds;
+
+    float leftPosX = 0f;
+    float rightPosX = 10f;
+    float xScreenHalfSize;
+    float yScreenHalfSize;
+
+    // private void Awake()
+    // {
+    //    viewHeight = Camera.main.orthographicSize * 2;
+    // }
+
+
+    // void Update()
+    // {
+    //    Vector3 curPos = transform.position;
+    //    Vector3 nextPos = Vector3.left * speed * Time.deltaTime;
+    //     transform.position = curPos + nextPos;
+
+    //    if (sprites[endIndex].position.y < viewHeight)
+    //    {
+    //        Vector3 backSpritePos = sprites[startIndex].localPosition;
+    //       Vector3 frontSpritePos = sprites[endIndex].localPosition;
+    //       sprites[endIndex].transform.localPosition = backSpritePos + Vector3.right * viewHeight;
+
+    //         int startIndexSave = startIndex;
+    //         startIndex = endIndex;
+    //         endIndex = (startIndexSave - 1 == -1) ? sprites.Length - 1 : startIndexSave - 1;
+    //    }
+    // }
+
+    void Start()
+    {
+        yScreenHalfSize = Camera.main.orthographicSize;
+        xScreenHalfSize = yScreenHalfSize * Camera.main.aspect;
+
+        leftPosX = -(xScreenHalfSize * 2);
+        rightPosX = xScreenHalfSize * 2 * backgrounds.Length;
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            backgrounds[i].position += new Vector3(-speed, 0, 0) * Time.deltaTime;
+
+            if (backgrounds[i].position.x < leftPosX)
+            {
+                Vector3 nextPos = backgrounds[i].position;
+                nextPos = new Vector3(nextPos.x + rightPosX, nextPos.y, nextPos.z);
+                backgrounds[i].position = nextPos;
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float maxSpeed;
+    public float jumpPower;
+    Rigidbody2D rigid;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        //Jump
+        if (Input.GetButtonDown("Jump"))
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        //stop speed
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+        }
+
+        
+    }
+
+
+    void FixedUpdate()
+    {
+        //Move By Key Control
+        float h = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        if (rigid.velocity.x > maxSpeed)    //Right Max Speed
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+
+        if (rigid.velocity.x < maxSpeed*(-1))    //Left Max Speed
+            rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+    }
+}
