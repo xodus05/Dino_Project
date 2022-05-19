9d206b37f6d618530fa0e4f872a5ce526abb1a3f
xodus05 <s2121@e-mirim.hs.kr>
2022-05-19 23:58:57 +0900

    오브젝트 사이즈 변경

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    bool isJump = false;
+    public float JP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJump = true;
+        }
+    }
+    private void FixedUpdate()
+    {
+        Jump();
+    }
+    void Jump()
+    {
+        if (!isJump)
+            return;
+
+        rigid.velocity = Vector2.zero;
+
+        Vector2 jumpVelocity = new Vector2(0, JP);
+        rigid.AddForce(jumpVelocity, ForceMode2D.Impulse);
+
+        isJump = false;
+    }
+}
