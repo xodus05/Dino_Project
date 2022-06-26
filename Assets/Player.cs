b54f3f13f074b47f442d88e49a767f09dcf16cef
xodus05 <s2121@e-mirim.hs.kr>
2022-06-27 00:23:16 +0900

    2단점프 수정

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,7 +6,7 @@ public class Player : MonoBehaviour
 {
     public float jumpPower = 5f;    //점프 높이
     public bool isGrounded = false;
-    public int jumpCount = 2; //점프횟수    2를 3으로 바꾸면 3단 점프
+    public int jumpCount; //점프횟수    2를 3으로 바꾸면 3단 점프
     Rigidbody2D rb;
     SpriteRenderer spriteRenderer;
     Animator anim;
@@ -17,7 +17,6 @@ public class Player : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
 
-        jumpCount = 0;
         isGrounded = true;
     }
 
@@ -35,6 +34,26 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        // 2단 점프
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.UpArrow) && jumpCount > 0)
+        {
+            rb.velocity = Vector2.up * jumpPower;
+        }
+
+        if (isGrounded == false && Input.GetKeyDown(KeyCode.UpArrow) && jumpCount > 0)
+        {
+            rb.velocity = Vector2.up * jumpPower;
+
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                rb.velocity = Vector2.up * jumpPower;
+                //rb.AddForce(new Vector2(0, 1) * jumpPower, ForceMode2D.Impulse); //위로 점프
+                anim.SetBool("isJump", true);
+                jumpCount--;    //점프할때 마다 점프횟수 감소
+            }
+        }
+
+
         //수그리기
         if (Input.GetKey(KeyCode.DownArrow))
         {
@@ -45,22 +64,5 @@ public class Player : MonoBehaviour
             anim.SetBool("isDown", false);
         }
 
-    }
-
-    void FixedUpdate()
-    {
-        //무한점프 막기
-        if (isGrounded)
-        {
-            if (jumpCount > 0)
-            {
-                if (Input.GetKeyDown(KeyCode.UpArrow))
-                {
-                    rb.AddForce(new Vector2(0, 1) * jumpPower, ForceMode2D.Impulse); //위로 점프
-                    anim.SetBool("isJump", true);
-                    jumpCount--;    //점프할때 마다 점프횟수 감소
-                }
-            }
-        }
     }
 }
